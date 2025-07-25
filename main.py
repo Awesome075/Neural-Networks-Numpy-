038e5316c06ef57a6a2db91857cbb8d6d1baedae
Adi Maqsood <adimaqsood1@gmail.com>
2025-07-25 23:20:34 +0530

    refactor: Replace XOR code with penguins dataset in main code

--- main.py
+++ main.py
@@ -2,17 +2,51 @@ import numpy as np
 from models import Model
 from layers import Dense
 from optimizers import SGD
+import pandas as pd
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder , StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-x_train = np.array([[0,0],[0,1],[1,0],[1,1]])
-y_train = np.array([[0],[1],[0],[1]])
+data=sns.load_dataset("penguins")
+data.dropna(inplace=True)
+
+le=LabelEncoder()
+non_numerical_cols = ['species','island','sex']
+
+for i in non_numerical_cols:
+    data[i] = le.fit_transform(data[i])
+
+X = data.drop('species' , axis=1)
+y = data.species
+
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 model = Model()
-model.add(Dense(n_dimension=4, n_inputs=2, activation='relu'))
+model.add(Dense(n_dimension=10, n_inputs=6, activation='relu'))
+model.add(Dense(n_dimension=3, n_inputs=10, activation='softmax'))
 
-optimizer = SGD(learning_rate=0.1)
+optimizer = SGD(learning_rate=0.005)
+model.compile(optimizer=optimizer, loss='sparse_cross_entropy')
 
-model.compile(optimizer=optimizer, loss='mse')
+#X_train = X_train.values.astype(np.float32)       
+y_train = y_train.values.astype(np.int32)
 print("Starting training...")
+model.fit(X_train, y_train, epochs=200)
 
-model.fit(x_train, y_train, epochs=10)
-print("Training finished.")
+print("Training finished.") 
+
+print("Testing...")
+#X_test = X_test.values.astype(np.float32)
+y_test = y_test.values.astype(np.int32)
+
+y_pred_probs = model.predict(X_test)
+y_pred = np.argmax(y_pred_probs, axis=1)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+
+print(y_test)
+print(y_pred)
